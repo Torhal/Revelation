48564c0dbc2e5db7d7b7ac76ddab87c7646cb870
torhal <torhal@8048255b-f405-41a1-95e1-685886f5e76f>
2009-02-06 01:31:56 +0000

    Added fourth argument to NewLocale().
    git-svn-id: svn+ssh://svn.wowace.com/wow/revelation/mainline/trunk@63 8048255b-f405-41a1-95e1-685886f5e76f

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -1,6 +1,12 @@
 -- English localization file for enUS and enGB.
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
-local L = AceLocale:NewLocale("Revelation", "enUS", true)
+
+local dev = false
+--@debug@
+dev = true
+--@end-debug@
+
+local L = AceLocale:NewLocale("Revelation", "enUS", true, dev)
 if not L then return end
 
 --@localization(locale="enUS", format="lua_additive_table", table_name="L", same-key-is-true=true)@
