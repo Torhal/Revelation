8425e4e8eacbf2860fa40c5446f597cccefaea15
torhal <torhal@8048255b-f405-41a1-95e1-685886f5e76f>
2010-03-01 18:59:34 +0000

    Added devel-enUS.lua
    git-svn-id: svn+ssh://svn.wowace.com/wow/revelation/mainline/trunk@130 8048255b-f405-41a1-95e1-685886f5e76f

---
+++ Localization/devel-enUS.lua
@@ -0,0 +1,28 @@
+-- English localization file for enUS and enGB.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+
+local L = AceLocale:NewLocale("Revelation", "enUS", true)
+
+if not L then return end
+
+L["Armor Enchantment"] = true
+L["Armor Vellum"] = true
+L["Boots"] = true
+L["Bracer"] = true
+L["Chest"] = true
+L["Cloak"] = true
+L["Create 1 - %d %s"] = true
+L["Create every %s you have reagents for."] = true
+L["Either no recipe or no reagents were found."] = true
+L["Gloves"] = true
+L["Ring"] = true
+L["Select the key to press when mouse-clicking for menu display."] = true
+L["Select the mouse button to click for menu display."] = true
+L["Select the second key to press when mouse-clicking for menu display."] = true
+L["Shield"] = true
+L["Staff"] = true
+L["Staves"] = true
+L["Trade Goods"] = true
+L["Weapon"] = true
+L["Weapon Enchantment"] = true
+L["Weapon Vellum"] = true
