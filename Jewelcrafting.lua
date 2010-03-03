8e017bf3675bf7df893b9eb0da5dcd9e5509d2f2
James D. Callahan III <darkenelf@gmail.com>
2010-03-02 19:35:59 -0500

    Only define CAN_PROSPECT if it's nil when common.CanProspect() is called.

--- Jewelcrafting.lua
+++ Jewelcrafting.lua
@@ -7,28 +7,27 @@ local ADDON_NAME, common = ...
 -------------------------------------------------------------------------------
 -- Constants
 -------------------------------------------------------------------------------
-local CAN_PROSPECT = {
-	[2770]	= true,	-- Copper Ore
-	[2771]	= true,	-- Tin Ore
-	[2772]	= true,	-- Iron Ore
-	[3858]	= true,	-- Mithril Ore
-	[10620]	= true,	-- Thorium Ore
-	[23452]	= true,	-- Adamantite Ore
-	[23424]	= true,	-- Fel Iron Ore
-	[36909]	= true,	-- Cobalt Ore
-	[36910]	= true,	-- Titanium Ore
-	[36912]	= true,	-- Saronite Ore
-}
-
--------------------------------------------------------------------------------
--- Variables.
--------------------------------------------------------------------------------
-
+local CAN_PROSPECT
 
 -------------------------------------------------------------------------------
 -- Functions.
 -------------------------------------------------------------------------------
 function common.CanProspect()
+	if not CAN_PROSPECT then
+		CAN_PROSPECT = {
+			[2770]	= true,	-- Copper Ore
+			[2771]	= true,	-- Tin Ore
+			[2772]	= true,	-- Iron Ore
+			[3858]	= true,	-- Mithril Ore
+			[10620]	= true,	-- Thorium Ore
+			[23452]	= true,	-- Adamantite Ore
+			[23424]	= true,	-- Fel Iron Ore
+			[36909]	= true,	-- Cobalt Ore
+			[36910]	= true,	-- Titanium Ore
+			[36912]	= true,	-- Saronite Ore
+		}
+	end
+
 	local id = select(3, common.cur_item.link:find("item:(%d+):"))
 
 	if not id or (id and not CAN_PROSPECT[tonumber(id)]) then
